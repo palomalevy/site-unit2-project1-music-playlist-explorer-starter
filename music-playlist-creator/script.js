5a9e6aee9977be256b66abc8ef6179ceb88ee74f
Paloma Levy <palomalevy@meta.com>
2025-06-04 17:57:43 -0700

    update website, changes to css

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,37 @@
+let playlists = document.querySelectorAll(".playlist")
 
+for (let playlist of playlists) {
+    playlist.addEventListener('click', function (event){
+        console.log(event.target)
+
+        let classTarget = event.target;
+        //console.log(classTarget.getElementById());
+
+        let popup = document.querySelector('.popup');
+
+        let title = playlist.querySelector('h2').innerText;
+        console.log(title);
+        let image = playlist.querySelector('img').src;
+        let createdBy = playlist.querySelector('p').innerText;
+
+        document.getElementById('Playlist Title').innerText = title;
+        document.getElementById('Playlist Image').src = image;
+        document.getElementById('Created By').innerText = createdBy;
+
+        popup.style.display = 'flex';
+    })
+}
+
+let closeButton = document.querySelector('.close');
+closeButton.addEventListener('click', function() {
+    let popup = document.querySelector('.popup');
+    popup.style.display = 'none';
+})
+
+function openFeatureTab() {
+    console.log('feature.click')
+}
+
+function openAllTab() {
+    console.log('feature.all')
+}
