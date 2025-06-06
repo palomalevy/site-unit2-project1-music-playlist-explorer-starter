71a1bd3fd6d1a9b86e27f55aa283f956794ea331
Paloma Levy <palomalevy@meta.com>
2025-06-05 20:20:18 -0700

    Everything is working now so I am updating this before I make some new changes

--- music-playlist-creator/data/data.js
+++ music-playlist-creator/data/data.js
@@ -1,32 +1,187 @@
-const songData = [
+const allPlaylists = [
   {
-    title: "Happy",
-    artist: "Pharrell Williams",
-    album: "G I R L",
-    duration: "3:53"
+    id: 1,
+    // coverImage: 'https://example.com/playlist1-cover.jpg',
+    title: 'My Favorite Songs',
+    creator: 'Created By: John Doe',
+    likes: 23,
+    songs: [
+      {
+        id: 1,
+        // image: 'https://example.com/song3-image.jpg',
+        title: "Pa' Que Retozen",
+        artist: 'Tego Calderón',
+        album: 'El Abayarde',
+        duration: '3:56'
+      },
+      {
+        id: 2,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      },
+      {
+        id: 3,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      },
+      {
+        id: 4,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      },
+    ]
   },
   {
-    title: "Uptown Funk",
-    artist: "Mark Ronson ft. Bruno Mars",
-    album: "Uptown Special",
-    duration: "4:38"
+    id: 2,
+    // coverImage: 'https://example.com/playlist2-cover.jpg',
+    title: 'Rock Classics',
+    creator: 'Created By: Jane Smith',
+    likes: 19,
+    songs: [
+      {
+        id: 1,
+        // image: 'https://example.com/song3-image.jpg',
+        title: 'Sweet Child O Mine',
+        artist: 'Guns N Roses',
+        album: 'Appetite for Destruction',
+        duration: '5:56'
+      },
+      {
+        id: 2,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      }
+    ]
   },
   {
-    title: "Can't Stop the Feeling!",
-    artist: "Justin Timberlake",
-    album: "Trolls: Original Motion Picture Soundtrack",
-    duration: "3:56"
+    id: 3,
+    // coverImage: 'https://example.com/playlist2-cover.jpg',
+    title: 'Reggaeton Classics',
+    creator: 'Created By: Paloma Levy',
+    likes: 34,
+    songs: [
+      {
+        id: 1,
+        // image: 'https://example.com/song3-image.jpg',
+        title: "Pa' Que Retozen",
+        artist: 'Tego Calderón',
+        album: 'El Abayarde',
+        duration: '3:56'
+      },
+      {
+        id: 2,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      }
+    ]
   },
+
   {
-    title: "We Found Love",
-    artist: "Rihanna ft. Calvin Harris",
-    album: "Talk That Talk",
-    duration: "3:35"
+    id: 3,
+    // coverImage: 'https://example.com/playlist2-cover.jpg',
+    title: 'Reggaeton Classics',
+    creator: 'Created By: Paloma Levy',
+    likes: 45,
+    songs: [
+      {
+        id: 1,
+        // image: 'https://example.com/song3-image.jpg',
+        title: "Pa' Que Retozen",
+        artist: 'Tego Calderón',
+        album: 'El Abayarde',
+        duration: '3:56'
+      },
+      {
+        id: 2,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      }
+    ]
   },
+
   {
-    title: "All About That Bass",
-    artist: "Meghan Trainor",
-    album: "Title",
-    duration: "3:08"
-  }
+    id: 3,
+    // coverImage: 'https://example.com/playlist2-cover.jpg',
+    title: 'Reggaeton Classics',
+    creator: 'Created By: Paloma Levy',
+    likes: 7,
+    songs: [
+      {
+        id: 1,
+        // image: 'https://example.com/song3-image.jpg',
+        title: "Pa' Que Retozen",
+        artist: 'Tego Calderón',
+        album: 'El Abayarde',
+        duration: '3:56'
+      },
+      {
+        id: 2,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      }
+    ]
+  },
+
+  {
+    id: 4,
+    // coverImage: 'https://example.com/playlist2-cover.jpg',
+    title: 'testings',
+    creator: 'Created By: Paloma Levy',
+    likes: 7,
+    songs: [
+      {
+        id: 1,
+        // image: 'https://example.com/song3-image.jpg',
+        title: "Pa' Que Retozen",
+        artist: 'Tego Calderón',
+        album: 'El Abayarde',
+        duration: '3:56'
+      },
+      {
+        id: 2,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      },
+      {
+        id: 3,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      },
+      {
+        id: 4,
+        // image: 'https://example.com/song4-image.jpg',
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        album: 'A Night at the Opera',
+        duration: '5:55'
+      },
+    ]
+  },
 ];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,45 +1,113 @@
-let playlists = document.querySelectorAll(".playlist")
+document.addEventListener("DOMContentLoaded", () => {
+  getPlaylistInformation();
+});
 
-for (let playlist of playlists) {
-    playlist.addEventListener('click', function (event){
-        console.log(event.target)
+function getSongInformation(playlistID) {
+    console.log('getting song info');
+    const foundPlaylist = allPlaylists.find(playlist => playlist.id === playlistID);
 
-        let classTarget = event.target;
-        //console.log(classTarget.getElementById());
+    let songHTML = ''
+    for(let songInfo of foundPlaylist.songs) {
+        songHTML += `
+            <div class="song">
+                        <div class="songImage">
+                            <img class="songImage" src="https://picsum.photos/200" alt="Song cover" width="100px">
+                        </div>
+                        <div class="songDetails">
+                            <h3 class="songTitle">${songInfo.title}</h3>
+                            <p class="artistName">${songInfo.artist}</p>
+                            <p class="albumTitle">${songInfo.album}</p>
+                            <p class="songDuration">${songInfo.duration}</p>
+                        </div>
+                    </div>
+        `
+    }
 
-        let popup = document.querySelector('.popup');
-
-        let title = playlist.querySelector('h2').innerText;
-        console.log(title);
-        let image = playlist.querySelector('img').src;
-        let createdBy = playlist.querySelector('p').innerText;
-
-        document.getElementById('Playlist Title').innerText = title;
-        document.getElementById('Playlist Image').src = image;
-        document.getElementById('Created By').innerText = createdBy;
-
-        // let popupTitle = playlist.querySelector('.popup-header h2');
-        // let popupImage = playlist.querySelector('.popup-header img');
-        // let popupCreator = playlist.querySelector('.popup-header p');
-
-        // if (popupTitle) popupTitle.innertext = title;
-        // if (popupImage) popupImage.src = image;
-        // if (popupCreator) popupCreator.innerText = createdBy
-
-        popup.style.display = 'flex';
-    })
+    document.querySelector('.allSongs').innerHTML = songHTML;
 }
 
-let closeButton = document.querySelector('.close');
-closeButton.addEventListener('click', function() {
-    let popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-})
+function getPlaylistInformation() {
+    console.log("getting information");
+    const playlistBody = document.getElementById("playlist-body");
+    allPlaylists.forEach((playlist) => {
+        console.log(playlist);
+        const playlistElem = createPlaylistElem(playlist);
+        console.log(playlistElem);
+        playlistBody.appendChild(playlistElem);
+    });
+
+    let playlists = document.querySelectorAll(".playlist");
+
+    for (let playlistElem of playlists) {
+        playlistElem.addEventListener("click", function (event) {
+            console.log(event.target);
+
+            if (event.target.classList.contains("heart-button")) {
+                event.stopPropagation();
+            } else {
+                let popup = document.querySelector(".popup");
+
+                let title = playlistElem.querySelector("h2").innerText;
+                console.log(title);
+                let image = playlistElem.querySelector("img").src;
+                let createdBy = playlistElem.querySelector("p").innerText;
+
+                document.getElementById("Playlist Title").innerText = title;
+                document.getElementById("Playlist Image").src = image;
+                document.getElementById("Created By").innerText = createdBy;
+
+                getSongInformation(parseInt(playlistElem.id));
+                console.log(playlistElem.id);
+                
+                popup.style.display = "flex";
+            }
+        });
+    }
+}
+
+function createPlaylistElem(playlist) {
+  console.log("creating playlist");
+  const article = document.createElement("article");
+  article.id = `${playlist.id}`
+  article.className = "playlist";
+  article.innerHTML = `
+    <img src="https://picsum.photos/200" alt="" width="200px">
+    <div class="playlist-name">
+        <h2>${playlist.title}</h2>
+        <p>${playlist.creator}</p>
+    </div>
+    <button class='heart-button' type="button">🤍
+        <div id='like-counter'>
+            <h3>${playlist.likes}</h3>
+        </div>
+    </button>
+    `;
+
+    // const heartButton = article.querySelector(".heart-button");
+    // const emojiSpan = heartButton.querySelector(".emoji");
+
+    // heartButton.addEventListener("click", () => {
+    //     if (emojiSpan.textContent === "🤍") {
+    //         emojiSpan.textContent = "💖";
+    //     } else {
+    //         emojiSpan.textContent = "🤍";
+    //     }
+    // });
+    
+
+  return article;
+}
+
+let closeButton = document.querySelector(".close");
+closeButton.addEventListener("click", function () {
+  let popup = document.querySelector(".popup");
+  popup.style.display = "none";
+});
 
 function openFeatureTab() {
-    console.log('feature.click')
+  console.log("feature.click");
 }
 
 function openAllTab() {
-    console.log('feature.all')
+  console.log("feature.all");
 }
